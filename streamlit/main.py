e6e182f009049747ca24b8261d36ea1e4454125c
root <root@p-bf1f20fb-a6c0-4ea2-8567-a5a41eea1d8a.projects-domain.projects.svc.cluster.local>
2022-06-14 14:57:11 +0000

    manual deepnote streamlit update commit

--- streamlit/main.py
+++ streamlit/main.py
@@ -149,45 +149,45 @@ def data_char():
     st.image(im, caption='Zipfs law graphs', width=400)
 
     import warnings
-warnings.filterwarnings('ignore')
+    warnings.filterwarnings('ignore')
 
-############### HATESPEECH Plots
-hs_wo_stopwords['idx'] = hs_wo_stopwords.index + 1
-hs_wo_stopwords['norm_freq'] = hs_wo_stopwords.frequency / len(hs_wo_stopwords)
-hs_wo_stopwords['cumul_frq'] = hs_wo_stopwords.norm_freq.cumsum()
+    ############### HATESPEECH Plots
+    hs_wo_stopwords['idx'] = hs_wo_stopwords.index + 1
+    hs_wo_stopwords['norm_freq'] = hs_wo_stopwords.frequency / len(hs_wo_stopwords)
+    hs_wo_stopwords['cumul_frq'] = hs_wo_stopwords.norm_freq.cumsum()
 
-sns.set()
-fig, axes = plt.subplots(2,3, figsize=(25,16))
-fig.suptitle("Corpus Frequent Word Statistics", size=30)
-sns.set_theme(style='whitegrid')
-plt.subplots_adjust(hspace = 0.3)
+    sns.set()
+    fig, axes = plt.subplots(2,3, figsize=(25,16))
+    fig.suptitle("Corpus Frequent Word Statistics", size=30)
+    sns.set_theme(style='whitegrid')
+    plt.subplots_adjust(hspace = 0.3)
 
-# axes[0,0].set_xscale('log')
-sns.scatterplot(ax=axes[0,0], x='idx', y='cumul_frq', data=hs_wo_stopwords).set_title("Hatespeech Cumulative frequency by index", size=18)
+    # axes[0,0].set_xscale('log')
+    sns.scatterplot(ax=axes[0,0], x='idx', y='cumul_frq', data=hs_wo_stopwords).set_title("Hatespeech Cumulative frequency by index", size=18)
 
-sns.lineplot(x='idx', y='cumul_frq', data=hs_wo_stopwords[:10000], ax=axes[0,1]).set_title("Hatespeech Cumulative frequency by index, top 10000 tokens", size=18)
+    sns.lineplot(x='idx', y='cumul_frq', data=hs_wo_stopwords[:10000], ax=axes[0,1]).set_title("Hatespeech Cumulative frequency by index, top 10000 tokens", size=18)
 
-hs_wo_stopwords['log_frq'] = np.log(hs_wo_stopwords.frequency)
-hs_wo_stopwords['log_rank'] = np.log(hs_wo_stopwords.frequency.rank(ascending=False))
-sns.regplot(x='log_rank', y='log_frq', data=hs_wo_stopwords, ax=axes[0,2], line_kws={"color": "red"}).set_title("Hatespeech Log-log plot for Zipf's law", size=18)
+    hs_wo_stopwords['log_frq'] = np.log(hs_wo_stopwords.frequency)
+    hs_wo_stopwords['log_rank'] = np.log(hs_wo_stopwords.frequency.rank(ascending=False))
+    sns.regplot(x='log_rank', y='log_frq', data=hs_wo_stopwords, ax=axes[0,2], line_kws={"color": "red"}).set_title("Hatespeech Log-log plot for Zipf's law", size=18)
 
-###################### EMOJI PLOTS
-#doing zipfs law on our frq dataframe and plotting
-emoji_wo_stopwords['idx'] = emoji_wo_stopwords.index + 1
-emoji_wo_stopwords['norm_freq'] = emoji_wo_stopwords.frequency / len(emoji_wo_stopwords)
-emoji_wo_stopwords['cumul_frq'] = emoji_wo_stopwords.norm_freq.cumsum()
+    ###################### EMOJI PLOTS
+    #doing zipfs law on our frq dataframe and plotting
+    emoji_wo_stopwords['idx'] = emoji_wo_stopwords.index + 1
+    emoji_wo_stopwords['norm_freq'] = emoji_wo_stopwords.frequency / len(emoji_wo_stopwords)
+    emoji_wo_stopwords['cumul_frq'] = emoji_wo_stopwords.norm_freq.cumsum()
 
-# Plots
-# axes[1,0].set_xscale('log')
-sns.scatterplot(ax=axes[1,0], x='idx', y='cumul_frq', data=emoji_wo_stopwords).set_title("Emoji Cumulative frequency by index", size=18)
+    # Plots
+    # axes[1,0].set_xscale('log')
+    sns.scatterplot(ax=axes[1,0], x='idx', y='cumul_frq', data=emoji_wo_stopwords).set_title("Emoji Cumulative frequency by index", size=18)
 
-sns.lineplot(x='idx', y='cumul_frq', data=emoji_wo_stopwords[:10000], ax=axes[1,1]).set_title("Emoji Cumulative frequency by index, top 10000 tokens", size=18)
+    sns.lineplot(x='idx', y='cumul_frq', data=emoji_wo_stopwords[:10000], ax=axes[1,1]).set_title("Emoji Cumulative frequency by index, top 10000 tokens", size=18)
 
-emoji_wo_stopwords['log_frq'] = np.log(emoji_wo_stopwords.frequency)
-emoji_wo_stopwords['log_rank'] = np.log(emoji_wo_stopwords.frequency.rank(ascending=False))
-sns.regplot(x='log_rank', y='log_frq', data=emoji_wo_stopwords, ax=axes[1,2], line_kws={"color": "red"}).set_title("Emoji Log-log plot for Zipf's law", size=18);
+    emoji_wo_stopwords['log_frq'] = np.log(emoji_wo_stopwords.frequency)
+    emoji_wo_stopwords['log_rank'] = np.log(emoji_wo_stopwords.frequency.rank(ascending=False))
+    sns.regplot(x='log_rank', y='log_frq', data=emoji_wo_stopwords, ax=axes[1,2], line_kws={"color": "red"}).set_title("Emoji Log-log plot for Zipf's law", size=18);
 
-return
+    return
 
 
 def man_anot():
@@ -202,6 +202,11 @@ def man_anot():
     
     dfcrowd = pd.read_csv("./streamlit/data/survey.csv")
     GT = pd.read_csv("./streamlit/data/GT.csv")
+    plt.plot(dfcrowd['ours'],label='Group annotation',linewidth=4.5)
+    plt.plot(sample100p,label='Original label',linewidth=4.5)
+
+#plt.plot(dfcrowd['annotation'],label='Crowd annotation')
+plt.legend()
     return 
 
 
