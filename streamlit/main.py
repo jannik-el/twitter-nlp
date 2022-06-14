f2c2c05fac80a8b8623c8deb469ae73895c665c1
root <root@p-bf1f20fb-a6c0-4ea2-8567-a5a41eea1d8a.projects-domain.projects.svc.cluster.local>
2022-06-14 09:28:26 +0000

    manual deepnote streamlit update commit

--- streamlit/main.py
+++ streamlit/main.py
@@ -55,7 +55,7 @@ def preprocessing():
     with st.expander("Tokenisation"):
         st.write("We created our own regex tokenizer and looked at how it worked compared to other tokenizers:")
 
-        line = st.text_input('Try it out below:', "#Fabulous evening tonight, can't wait to be at home again!! 😍")
+        line = st.text_input('Try it out below:', "#Fabulous evening tonight, I'm so @ home😍")
         
         tokenizers = ["Regex", "NLTKTweetModified", "NLTKTweet", "NLTKTreeBank"]
 
