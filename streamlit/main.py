f88090012829c3762fa3364fe2e74dcc9f90daa2
root <root@p-bf1f20fb-a6c0-4ea2-8567-a5a41eea1d8a.projects-domain.projects.svc.cluster.local>
2022-06-14 12:27:11 +0000

    manual deepnote streamlit update commit

--- streamlit/main.py
+++ streamlit/main.py
@@ -111,9 +111,8 @@ def data_char():
 
     col1, col2 = st.columns(2)
     with col1:
-        
         fig = plt.figure(figsize = (5,10))
-        hsw_stopwords.iloc[0:30].plot.barh(x='token',y='frequency', figsize=(5,13))
+        hsw_stopwords.iloc[0:30].plot.barh(x='token',y='frequency', figsize=(5,14))
         plt.title("Most frequent words in hatespeech dataset (top 50) without stopwords")
         plt.xticks(rotation = 90)
         st.pyplot(fig=plt)
@@ -121,7 +120,6 @@ def data_char():
         custom_wc(hs_wo_stopwords_dict_lf)
         
     with col2: 
-        
         fig = plt.figure(figsize = (5,10))
         emoji_stopwords.iloc[0:30].plot.barh(x='token',y='frequency', figsize=(5,13))
         plt.title("Most frequent words in emoji dataset (top 50) without stopwords")
