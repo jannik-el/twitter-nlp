da28813c6fb1cfe8c24cee49786fa3ad185ffee4
root <root@p-bf1f20fb-a6c0-4ea2-8567-a5a41eea1d8a.projects-domain.projects.svc.cluster.local>
2022-06-14 12:11:35 +0000

    manual deepnote streamlit update commit

--- streamlit/main.py
+++ streamlit/main.py
@@ -123,9 +123,13 @@ def data_char():
     hs_wo_stopwords_dict_lf = dict((k, v) for k, v in hs_wo_stopwords_dict.items() if v <= 3) #least frequent hate (value <= 3)
     emoji_wo_stopwords_dict_lf = dict((k,v) for k, v in emoji_wo_stopwords_dict.items() if v <= 3) #least frequent emoji (value <=3)
 
-    custom_wc(hs_wo_stopwords_dict_lf)
-    custom_wc(emoji_wo_stopwords_dict_lf)
-    
+    col1, col2 = st.columns(2)
+
+    with col1:
+        custom_wc(hs_wo_stopwords_dict_lf)
+
+    with col2:
+        custom_wc(emoji_wo_stopwords_dict_lf)
     return
 
 
