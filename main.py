b3a4eb3c3326d65452fa6722163ebd70ce125a50
root <root@p-bf1f20fb-a6c0-4ea2-8567-a5a41eea1d8a.projects-domain.projects.svc.cluster.local>
2022-06-15 09:47:05 +0000

    manual deepnote streamlit update commit

--- main.py
+++ main.py
@@ -334,14 +334,14 @@ def auto_predic():
     with col1:
         fig1 = plt.figure(figsize = (3,3))
         y_train_counts = open_jar("./streamlit/ytraincounts.pkl")
-        plt.rcParams['font.size'] = 4.0
+        plt.rcParams['font.size'] = 0.0
         plt.pie(collections.Counter(list(y_train_counts)).values(), labels=['Not hate speech','Hate speech'],colors=['#eab676','#2596be'],explode=(0, 0.1), autopct = lambda p:f'{p:.2f}%')
         st.pyplot(fig1=plt)
     with col2:
         fig2 = plt.figure(figsize = (15,15))
         y_train_emoji_counts = open_jar("./streamlit/ytrainemojicounts.pkl")
         emoji_classes= pd.read_csv("./streamlit/data/mapping-2.txt", sep = "	", header=None)
-        plt.rcParams['font.size'] = 20.0
+        plt.rcParams['font.size'] = 15.0
         plt.pie(collections.Counter(list(y_train_emoji_counts)).values(), labels=list(emoji_classes[2]), autopct = lambda p:f'{p:.2f}%');
         st.pyplot(fig2=plt)
 
