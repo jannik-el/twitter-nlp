fef18b8f0d30df2562d8ab3eee9bed3c2889ca42
root <root@p-bf1f20fb-a6c0-4ea2-8567-a5a41eea1d8a.projects-domain.projects.svc.cluster.local>
2022-06-15 11:04:47 +0000

    manual deepnote streamlit update commit

--- main.py
+++ main.py
@@ -377,22 +377,6 @@ def auto_predic():
     scores = scores[["Classifier", "F1 score", "Accuracy Score", "Recall Score", "Precision Score"]]
     st.table(scores)
     
-    st.write("------------------------------------------------------------------")
-    col1, col2 = st.columns(2)
-    with col1:
-        fig1 = plt.figure(figsize = (3,3))
-        y_train_counts = open_jar("./streamlit/ytraincounts.pkl")
-        plt.rcParams['font.size'] = 11.0
-        plt.pie(collections.Counter(list(y_train_counts)).values(), labels=['Not hate speech','Hate speech'],colors=['#eab676','#2596be'],explode=(0, 0.1), autopct = lambda p:f'{p:.2f}%')
-        st.pyplot(fig1=plt)
-    with col2:
-        fig2 = plt.figure(figsize = (15,15))
-        y_train_emoji_counts = open_jar("./streamlit/ytrainemojicounts.pkl")
-        emoji_classes= pd.read_csv("./streamlit/data/mapping-2.txt", sep = "	", header=None)
-        plt.rcParams['font.size'] = 11.0
-        plt.pie(collections.Counter(list(y_train_emoji_counts)).values(), labels=list(emoji_classes[2]), autopct = lambda p:f'{p:.2f}%');
-        st.pyplot(fig2=plt)
-    
     return
 
 def data_aug():
@@ -465,6 +449,8 @@ def data_aug():
     st.pyplot()
 
     st.markdown("### Looking more closely however, we can say that:")
+
+    
 
     
 
